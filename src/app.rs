fda7e2af32e74fde25ce72b4247dd6a616729fc5
Samuel Brolia <samuel_brolia@msn.com>
2020-10-04 01:23:34 +0100

    simplify

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,13 @@
 use yew::prelude::*;
-use rand::prelude::*;
-use yew::services::{ConsoleService, DialogService};
-
-
-pub struct App {
-    items: Vec<i64>,
-    link: ComponentLink<Self>,
-    console: ConsoleService,
-    dialog: DialogService,
-}
 
 pub enum Msg {
     AddOne,
     RemoveOne,
-    About
+}
+
+pub struct App {
+    items: Vec<i64>,
+    link: ComponentLink<Self>,
 }
 
 impl Component for App {
@@ -21,43 +15,20 @@ impl Component for App {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        App { 
-            link, 
-            items: Vec::new(), 
-            console: ConsoleService::new(), 
-            dialog: DialogService::new(),
+        App {
+            link,
+            items: Vec::new(),
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::About => self.dialog.alert("Purposeless App"),
             Msg::AddOne => {
-                let added: i64 = random();
-                self.items.push(added);
-                self.console.log(format!("Added: {}", added).as_str());
-                self.console.info("Added 1 elemet to the vec");
+                self.items.push(1);
             }
             Msg::RemoveOne => {
-                let removed = self.items.pop();
-                match removed {
-                    Some(x) => self.console.warn(format!("Removed {}", x).as_str()),
-                    None => {
-                        self.console.error("No more elements to remove!");
-                        let user_is_a_monkey = self
-                            .dialog
-                            .confirm("Are you dum? There are no more elements to remove!");
-                        if user_is_a_monkey {
-                            self.dialog.alert("I kenw it!");
-                        } else {
-                            self.dialog.alert(
-                                "Maybe it was an error, there are no more elements to remove!",
-                            );
-                        }
-                    }
-                };
+                let _ = self.items.pop();
             }
-            
         }
         true
     }  
@@ -75,7 +46,6 @@ impl Component for App {
                 <div class="card">
                     <header>
                         <h2>{"Items: "}</h2>
-                        <button onclick=self.link.callback(|_| Msg::About)>{ "About" }</button>
                     </header>
                     <div class="card-body">
                         <table class="primary">
